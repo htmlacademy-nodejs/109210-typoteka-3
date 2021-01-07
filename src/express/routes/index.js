fae193325fb4625036805681b4b9613e15a80d9b
IrinaZorkova <irinanazorkova@gmail.com>
2021-01-07 14:20:49 +0300

    routing

---
+++ src/express/routes/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mainRoutes = require(`./main.routes`);
+const myRoutes = require(`./my.routes`);
+const articlesRoutes = require(`./articles.routes`);
+
+module.exports = {
+  mainRoutes,
+  myRoutes,
+  articlesRoutes
+};
